7e3df9669ed65f112db01c28ede00b833d596a80
astroboy20 <tolulopeakinkunmi7@gmail.com>
2023-01-04 17:02:30 +0100

    update

---
+++ liveUserFilter/data.js
@@ -0,0 +1,62 @@
+export const Data = [
+    {
+        id:1,
+        name:'Charles Carling',
+        location:'Nigeria',
+        age:'32'
+    },
+    {
+        id:1,
+        name:'Charles Carling',
+        location:'Nigeria',
+        age:'32'
+    },
+    {
+        id:1,
+        name:'Charles Carling',
+        location:'Nigeria',
+        age:'32'
+    },
+    {
+        id:1,
+        name:'Charles Carling',
+        location:'Nigeria',
+        age:'32'
+    },
+    {
+        id:1,
+        name:'Charles Carling',
+        location:'Nigeria',
+        age:'32'
+    },
+    {
+        id:1,
+        name:'Charles Carling',
+        location:'Nigeria',
+        age:'32'
+    },
+    {
+        id:1,
+        name:'Charles Carling',
+        location:'Nigeria',
+        age:'32'
+    },
+    {
+        id:1,
+        name:'Charles Carling',
+        location:'Nigeria',
+        age:'32'
+    },
+    {
+        id:1,
+        name:'Charles Carling',
+        location:'Nigeria',
+        age:'32'
+    },
+    {
+        id:1,
+        name:'Charles Carling',
+        location:'Nigeria',
+        age:'32'
+    }
+]
